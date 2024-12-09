f9f3994b5cdf5ed3f26fe1c72b28b18b5d6f6237
Hlofiys <hlofiys@gmail.com>
2024-12-09 22:01:55 +0300

    get rid of unused stuff

---
+++ tofu/main.tf
@@ -0,0 +1,95 @@
+module "talos" {
+  source = "./talos"
+
+  providers = {
+    proxmox = proxmox
+  }
+
+  image = {
+    version        = "v1.8.3"
+    schematic      = file("${path.module}/talos/image/schematic.yaml")
+  }
+
+  cilium = {
+    values  = file("${path.module}/../k8s/infra/network/cilium/values.yaml")
+    install = file("${path.module}/talos/inline-manifests/cilium-install.yaml")
+  }
+
+  cluster = {
+    name            = "talos"
+    endpoint        = "192.168.1.100"
+    gateway         = "192.168.1.1"
+    talos_version   = "v1.8"
+    proxmox_cluster = "homelab"
+  }
+
+  nodes = {
+    "ctrl-00" = {
+      host_node     = "sugoma"
+      machine_type  = "controlplane"
+      ip            = "192.168.1.100"
+      mac_address   = "BC:24:11:2E:C8:00"
+      vm_id         = 800
+      cpu           = 4
+      ram_dedicated = 4096
+    }
+    "work-00" = {
+      host_node     = "sugoma"
+      machine_type  = "worker"
+      ip            = "192.168.1.110"
+      mac_address   = "BC:24:11:2E:A8:00"
+      vm_id         = 810
+      cpu           = 4
+      ram_dedicated = 4096
+    }
+  }
+
+}
+
+module "sealed_secrets" {
+  depends_on = [module.talos]
+  source     = "./bootstrap/sealed-secrets"
+
+  providers = {
+    kubernetes = kubernetes
+  }
+
+  // openssl req -x509 -days 365 -nodes -newkey rsa:4096 -keyout sealed-secrets.key -out sealed-secrets.cert -subj "/CN=sealed-secret/O=sealed-secret"
+  cert = {
+    cert = file("${path.module}/bootstrap/sealed-secrets/certificate/sealed-secrets.cert")
+    key  = file("${path.module}/bootstrap/sealed-secrets/certificate/sealed-secrets.key")
+  }
+}
+
+module "proxmox_csi_plugin" {
+  depends_on = [module.talos]
+  source     = "./bootstrap/proxmox-csi-plugin"
+
+  providers = {
+    proxmox    = proxmox
+    kubernetes = kubernetes
+  }
+
+  proxmox = var.proxmox
+}
+
+module "volumes" {
+  depends_on = [module.proxmox_csi_plugin]
+  source     = "./bootstrap/volumes"
+
+  providers = {
+    restapi    = restapi
+    kubernetes = kubernetes
+  }
+  proxmox_api = var.proxmox
+  volumes = {
+    pv-postgres = {
+      node = "sugoma"
+      size = "4G"
+    }
+    pv-prometheus = {
+      node = "sugoma"
+      size = "4G"
+    }
+  }
+}
